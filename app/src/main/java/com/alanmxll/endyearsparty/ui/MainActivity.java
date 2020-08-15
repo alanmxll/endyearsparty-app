781a797f4b9a2f383c3860a78d76f138f71c90bc
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-08-15 20:12:50 -0300

    obtain the confirmation on MainActivity

--- app/src/main/java/com/alanmxll/endyearsparty/ui/MainActivity.java
+++ app/src/main/java/com/alanmxll/endyearsparty/ui/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alanmxll.endyearsparty.R;
+import com.alanmxll.endyearsparty.constant.EndYearConstants;
+import com.alanmxll.endyearsparty.data.SecurityPreferences;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,12 +18,15 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
     private static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mSecurityPreferences = new SecurityPreferences(this);
 
         this.mViewHolder.textToday = findViewById(R.id.text_today);
         this.mViewHolder.textDaysLeft = findViewById(R.id.text_days_left);
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.mViewHolder.textToday.setText(SIMPLE_DATE_FORMAT.format(Calendar.getInstance().getTime()));
         String daysLeft = String.format("%s %s", this.getDaysLeft(), getString(R.string.dias));
         this.mViewHolder.textDaysLeft.setText(daysLeft);
+
+        this.verifyPresence();
     }
 
     @Override
@@ -40,6 +47,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.button_confirm) {
             Intent intent = new Intent(this, DetailsActivity.class);
             startActivity(intent);
+        }
+    }
+
+    private void verifyPresence() {
+        String presence = this.mSecurityPreferences.getStoredString(EndYearConstants.PRESENCE_KEY);
+
+        if (presence.equals("")) {
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.not_confirmed));
+        } else if (presence.equals(EndYearConstants.CONFIRMATION_YES)) {
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.yes));
+        } else {
+            this.mViewHolder.buttonConfirm.setText(getString(R.string.no));
         }
     }
 
